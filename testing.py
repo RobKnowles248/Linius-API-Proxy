e1d51de59c8b4d2307ceeae8adc4c2cf2c2fb3f6
Rob Knowles <robert@knowles-family.me.uk>
2021-01-14 13:38:31 +0000

    Test API_key addition

--- testing.py
+++ testing.py
@@ -5,5 +5,5 @@ from proxy import Proxy
 if os.path.exists("env.py"):
     import env
 
-robs_proxy = Proxy("robknowles", os.environ.get("password"))
+robs_proxy = Proxy("robknowles", os.environ.get("password"), os.environ.get("API_key"))
 print(robs_proxy.token)
