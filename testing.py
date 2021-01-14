93b1ecabe4975d20461c9523dc9c6a56dfb8ade4
Rob Knowles <robert@knowles-family.me.uk>
2021-01-14 19:34:52 +0000

    Reformat testing.py to make tests more readable

--- testing.py
+++ testing.py
@@ -6,20 +6,32 @@ if os.path.exists("env.py"):
     import env
 
 # Call the proxy
-robs_proxy = Proxy("robknowles", os.environ.get("password"), os.environ.get("API_key"))
+test_proxy = Proxy("robknowles", os.environ.get("password"), os.environ.get("API_key"))
 
 # Testing the init method
-# print(robs_proxy.token)
-# print(robs_proxy.headers)
+test_token = test_proxy.token
+print(f"Token: {test_token}")
+
+test_headers = test_proxy.headers
+print("")
+print(f"Headers: \n{test_headers}")
 
 # Testing discover method
-# print(robs_proxy.discover("https://linius-lvs.s3.amazonaws.com/demo/tears-of-steel.mp4"))
+print("")
+test_discover = test_proxy.discover("https://linius-lvs.s3.amazonaws.com/demo/tears-of-steel.mp4")
+print(f"Discover method output: \n{test_discover}")
 
 # Testing enrich assets method
-# print(robs_proxy.enrich_assets("624311", 2))
+print("")
+test_enrich_assets = test_proxy.enrich_assets("624311", 2)
+print(f"Enrich assets method output: \n{test_enrich_assets}")
 
 # Testing enrich jobs method
-# print(robs_proxy.enrich_jobs(["624311"]))
+print("")
+test_enrich_jobs = test_proxy.enrich_jobs(["624311"])
+print(f"Enrich jobs method output: \n{test_enrich_jobs}")
 
 # Testing search method
-print(robs_proxy.search("tears"))
+print("")
+test_search = test_proxy.search("tears")
+print(f"Search method output: \n{test_search}")
