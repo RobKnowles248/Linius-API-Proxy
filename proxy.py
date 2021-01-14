a2d1163a98599a84ee961bb6550d3d2a5724a40e
Rob Knowles <robert@knowles-family.me.uk>
2021-01-14 11:52:47 +0000

    Signin to linius API

--- proxy.py
+++ proxy.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import requests
+import json
+import os
+if os.path.exists("env.py"):
+    import env
+
+signin_data = {"password": os.environ.get("password"),"userName": "robknowles"}
+signin_request = requests.post("https://api.lvs.linius.com/v2/iam/auth/signin", json=signin_data)
+print(signin_request.content)
+
