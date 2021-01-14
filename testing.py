ca36cbcc4356f84eba52ca8e18fb66f93adfd864
Rob Knowles <robert@knowles-family.me.uk>
2021-01-14 14:18:38 +0000

    Test enrich_assets() method

--- testing.py
+++ testing.py
@@ -9,8 +9,11 @@ if os.path.exists("env.py"):
 robs_proxy = Proxy("robknowles", os.environ.get("password"), os.environ.get("API_key"))
 
 # Testing the init method
-print(robs_proxy.token)
-print(robs_proxy.headers)
+# print(robs_proxy.token)
+# print(robs_proxy.headers)
 
 # Testing discover method
-print(robs_proxy.discover("https://linius-lvs.s3.amazonaws.com/demo/tears-of-steel.mp4"))
+# print(robs_proxy.discover("https://linius-lvs.s3.amazonaws.com/demo/tears-of-steel.mp4"))
+
+# Testing enrich assets method
+print(robs_proxy.enrich_assets("624311", 2))
