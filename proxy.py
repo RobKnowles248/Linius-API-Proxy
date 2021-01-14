7382a1145403b0890404c08afa1a71627359bd2a
Rob Knowles <robert@knowles-family.me.uk>
2021-01-14 13:14:57 +0000

    Turn proxy into a class

--- proxy.py
+++ proxy.py
@@ -1,28 +1,26 @@
 import requests
 import json
 import os
-if os.path.exists("env.py"):
-    import env
 
-def signin(userName, password):
-    """
-    Function that signs into the Linius API and returns an oAuth code.
-    """
-    signin_data = {"password": password, "userName": userName}
-    signin_request = requests.post("https://api.lvs.linius.com/v2/iam/auth/signin", json=signin_data)
-    return signin_request.json()["token"]
 
-my_token = signin("robknowles", os.environ.get("password"))
+class Proxy():
 
-def validateToken(token):
-    """
-    Function that validates an oAuth token for the Linius API
-    """
-    headers = {"authorization": "Bearer " + token}
-    validate_request = requests.get("https://api.lvs.linius.com/v2/iam/auth/token/validate", headers=headers)
-    if validate_request.status_code == 200:
-        return "Valid"
-    elif validate_request.status_code == 401:
-        return "Invalid"
+    def __init__(self, userName, password):
+        """
+        Method that creates a class variable of token which gives the oAuth token
+        """
+        signin_data = {"password": password, "userName": userName}
+        signin_request = requests.post("https://api.lvs.linius.com/v2/iam/auth/signin", json=signin_data)
+        self.token = signin_request.json()["token"]
+        
 
-print(validateToken(my_token))
+    def validateToken(self):
+        """
+        Function that validates an oAuth token for the Linius API
+        """
+        headers = {"authorization": "Bearer " + self.token}
+        validate_request = requests.get("https://api.lvs.linius.com/v2/iam/auth/token/validate", headers=headers)
+        if validate_request.status_code == 200:
+            return "Valid"
+        elif validate_request.status_code == 401:
+            return "Invalid"
