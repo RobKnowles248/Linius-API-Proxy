1c6f45edeb8648837d53ac60cbfc3e11fc47ab1e
Rob Knowles <robert@knowles-family.me.uk>
2021-01-14 13:50:44 +0000

    Tested the discover method

--- testing.py
+++ testing.py
@@ -5,6 +5,17 @@ from proxy import Proxy
 if os.path.exists("env.py"):
     import env
 
+# Call the proxy
 robs_proxy = Proxy("robknowles", os.environ.get("password"), os.environ.get("API_key"))
-print(robs_proxy.token)
-print(robs_proxy.headers)
+
+# Testing the init method
+#print(robs_proxy.token)
+#print(robs_proxy.headers)
+
+# Testing discover method
+print(robs_proxy.discover(
+    "Tears of Steel",
+    "https://linius-lvs.s3.amazonaws.com/demo/tears-of-steel.mp4",
+    ["Tears of Steel", "Demo5"],
+    "http://www.gstatic.com/tv/thumb/movieposters/10875273/p10875273_p_v8_ac.jpg",
+    False))
